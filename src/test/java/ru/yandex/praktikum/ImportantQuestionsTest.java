b03972807f11beee36151d10fe9d8dc74c6d46f4
Sofya Glazunova <profhom@yandex.ru>
2025-04-16 11:48:26 +0300

    2 доработка

--- src/test/java/ru/yandex/praktikum/ImportantQuestionsTest.java
+++ src/test/java/ru/yandex/praktikum/ImportantQuestionsTest.java
@@ -19,44 +19,44 @@ public class ImportantQuestionsTest {
     private WebDriver driver;
     private BasicUtils utils;
 
-    private final int INDEXOfROW;
-    private final String EXPECTED_STRING;
+    private final int INDEX_OF_ANSWER;
+    private final String EXPECTED_ANSWER;
 
-    private static final String EXPECTED_STRING_1 = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
-    private static final String EXPECTED_STRING_2 = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
-    private static final String EXPECTED_STRING_3 = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
-    private static final String EXPECTED_STRING_4 = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
-    private static final String EXPECTED_STRING_5 = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
-    private static final String EXPECTED_STRING_6 = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
-    private static final String EXPECTED_STRING_7 = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
-    private static final String EXPECTED_STRING_8 = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+    private static final String COST_ANSWER = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+    private static final String SCOOTER_COUNT_ANSWER = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+    private static final String RENTAL_TIME_ANSWER = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+    private static final String ORDER_TIME_ANSWER = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+    private static final String ORDER_EXTENSION_ANSWER = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+    private static final String CHARGING_ANSWER = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+    private static final String CANCELLATION_ORDER_ANSWER = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+    private static final String MOSCOW_REGION_ORDER_ANSWER = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
 
-    private static final int INDEXOfROW1 = 0;
-    private static final int INDEXOfROW2 = 1;
-    private static final int INDEXOfROW3 = 2;
-    private static final int INDEXOfROW4 = 3;
-    private static final int INDEXOfROW5 = 4;
-    private static final int INDEXOfROW6 = 5;
-    private static final int INDEXOfROW7 = 6;
-    private static final int INDEXOfROW8 = 7;
+    private static final int INDEX_OF_COST_ANSWER = 0;
+    private static final int INDEX_OF_SCOOTER_COUNT_ANSWER = 1;
+    private static final int INDEX_OF_RENTAL_TIME_ANSWER = 2;
+    private static final int INDEX_OF_ORDER_TIME_ANSWER = 3;
+    private static final int INDEX_OF_ORDER_EXTENSION_ANSWER = 4;
+    private static final int INDEX_OF_CHARGING_ANSWER = 5;
+    private static final int INDEX_OF_CANCELLATION_ORDER_ANSWER = 6;
+    private static final int INDEX_OF_MOSCOW_REGION_ORDER_ANSWER = 7;
 
 
-    public ImportantQuestionsTest(int INDEXOfROW, String EXPECTED_STRING) {
-        this.INDEXOfROW = INDEXOfROW;
-        this.EXPECTED_STRING = EXPECTED_STRING;
+    public ImportantQuestionsTest(int INDEX_OF_ANSWER, String EXPECTED_ANSWER) {
+        this.INDEX_OF_ANSWER = INDEX_OF_ANSWER;
+        this.EXPECTED_ANSWER = EXPECTED_ANSWER;
     }
 
     @Parameterized.Parameters(name = "Тест строки с индексом {0}")
     public static Object[][] setRows() {
         return new Object[][]{
-                {INDEXOfROW1, EXPECTED_STRING_1},
-                {INDEXOfROW2, EXPECTED_STRING_2},
-                {INDEXOfROW3, EXPECTED_STRING_3},
-                {INDEXOfROW4, EXPECTED_STRING_4},
-                {INDEXOfROW5, EXPECTED_STRING_5},
-                {INDEXOfROW6, EXPECTED_STRING_6},
-                {INDEXOfROW7, EXPECTED_STRING_7},
-                {INDEXOfROW8, EXPECTED_STRING_8},
+                {INDEX_OF_COST_ANSWER, COST_ANSWER},
+                {INDEX_OF_SCOOTER_COUNT_ANSWER, SCOOTER_COUNT_ANSWER},
+                {INDEX_OF_RENTAL_TIME_ANSWER, RENTAL_TIME_ANSWER},
+                {INDEX_OF_ORDER_TIME_ANSWER, ORDER_TIME_ANSWER},
+                {INDEX_OF_ORDER_EXTENSION_ANSWER, ORDER_EXTENSION_ANSWER},
+                {INDEX_OF_CHARGING_ANSWER, CHARGING_ANSWER},
+                {INDEX_OF_CANCELLATION_ORDER_ANSWER, CANCELLATION_ORDER_ANSWER},
+                {INDEX_OF_MOSCOW_REGION_ORDER_ANSWER, MOSCOW_REGION_ORDER_ANSWER},
         };
     }
 
@@ -74,9 +74,9 @@ public class ImportantQuestionsTest {
         driver.get(objMainPage.MAINPAGE_URL);
         objMainPage.clickAcceptCookies();
 
-        WebElement answerElement = objMainPage.getAnswer(INDEXOfROW);
+        WebElement answerElement = objMainPage.getAnswer(INDEX_OF_ANSWER);
         String actualText = answerElement.getText();
-        assertEquals("Неверный текст в строке с индексом " + INDEXOfROW, EXPECTED_STRING, actualText);
+        assertEquals("Неверный текст в строке с индексом " + INDEX_OF_ANSWER, EXPECTED_ANSWER, actualText);
     }
 
 
